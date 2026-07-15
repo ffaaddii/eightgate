4e7ecd107301baceb3bfd283d83a39999692e268
unknown <fadiabunaser2002@gmail.com>
2026-07-15 14:37:13 +0300

    hi

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ import app from './app.js';
 /**
  * start server with port
  */
-const PORT = parseInt(process.env.PORT || '3001', 10);
+const PORT = parseInt(process.env.PORT || '5151', 10);
 const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server ready on port ${PORT}`);
 });
